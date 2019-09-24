104b778ed7f67f56fe8459deb3abb5e2bb0eea95
Booyoun-Kim <booyoun@wannabit.io>
2019-09-24 13:56:23 +0900

    Added code about exception of IRIS(MsgDelegate). Added comment. Updated keystation window popup position for multiple monitors.

--- www/lib/keystation.js
+++ www/lib/keystation.js
@@ -1,3 +1,26 @@
+function PopupCenter(url, title, w, h, opts) {
+	var _innerOpts = '';
+	if (opts !== null && typeof opts === 'object' ) {
+		for (var p in opts ) {
+			if (opts.hasOwnProperty(p)) {
+				_innerOpts += p + '=' + opts[p] + ',';
+			}
+		}
+	}
+	// Fixes dual-screen position, Most browsers, Firefox
+	var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+	var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+	var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+	var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+	var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+	var top = ((height / 2) - (h / 2)) + dualScreenTop;
+	var newWindow = window.open(url, title, _innerOpts + ' width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+	return newWindow;
+}
+
 function Keystation(client, lcd, path) {
 	this.client 	= client;
 	this.lcd		= lcd;
@@ -8,11 +31,6 @@ function Keystation(client, lcd, path) {
 }
 
 Keystation.prototype.openWindow = function(type, payload) {
-	const w = 400
-  	const h = 690
-	var left = (screen.width/2)-(w/2);
-	var top = (screen.height/2)-(h/2);
-
 	var apiUrl = "";
 	switch (type) {
 		case "signin":
@@ -23,5 +41,5 @@ Keystation.prototype.openWindow = function(type, payload) {
 			break;
 	}
 
-	return window.open(this.keystationUrl + '/' + apiUrl + '?client=' + encodeURIComponent(this.client) + '&lcd=' + encodeURIComponent(this.lcd) + '&path=' + encodeURIComponent(this.path) + '&payload=' + encodeURIComponent(payload), '', "width=" + w + ", height=" + h + " , top=" + top + ", left=" + left);
+	return PopupCenter(this.keystationUrl + '/' + apiUrl + '?client=' + encodeURIComponent(this.client) + '&lcd=' + encodeURIComponent(this.lcd) + '&path=' + encodeURIComponent(this.path) + '&payload=' + encodeURIComponent(payload),'','400','690', {toolbar:1, resizable:1, location:1, menubar:1, status:1});
 }
