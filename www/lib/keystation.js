68f5c490f061700d42454b467a1765123c091469
Booyoun-Kim <booyoun@wannabit.io>
2020-02-25 17:21:16 +0900

    Updated position of popup window

--- www/lib/keystation.js
+++ www/lib/keystation.js
@@ -1,23 +1,40 @@
-function PopupCenter(url, title, w, h, opts) {
-	var _innerOpts = '';
-	if (opts !== null && typeof opts === 'object' ) {
-		for (var p in opts ) {
-			if (opts.hasOwnProperty(p)) {
-				_innerOpts += p + '=' + opts[p] + ',';
-			}
-		}
+// Find Left Boundry of the Screen/Monitor
+function FindLeftScreenBoundry()
+{
+	// Check if the window is off the primary monitor in a positive axis
+	// X,Y                  X,Y                    S = Screen, W = Window
+	// 0,0  ----------   1280,0  ----------
+	//     |          |         |  ---     |
+	//     |          |         | | W |    |
+	//     |        S |         |  ---   S |
+	//      ----------           ----------
+	if (window.leftWindowBoundry() > window.screen.width)
+	{
+		return window.leftWindowBoundry() - (window.leftWindowBoundry() - window.screen.width);
 	}
-	// Fixes dual-screen position, Most browsers, Firefox
-	var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
-	var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
 
-	var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-	var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+	// Check if the window is off the primary monitor in a negative axis
+	// X,Y                  X,Y                    S = Screen, W = Window
+	// 0,0  ----------  -1280,0  ----------
+	//     |          |         |  ---     |
+	//     |          |         | | W |    |
+	//     |        S |         |  ---   S |
+	//      ----------           ----------
+	// This only works in Firefox at the moment due to a bug in Internet Explorer opening new windows into a negative axis
+	// However, you can move opened windows into a negative axis as a workaround
+	if (window.leftWindowBoundry() < 0 && window.leftWindowBoundry() > (window.screen.width * -1))
+	{
+		return (window.screen.width * -1);
+	}
 
-	var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-	var top = ((height / 2) - (h / 2)) + dualScreenTop;
-	var newWindow = window.open(url, title, _innerOpts + ' width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+	// If neither of the above, the monitor is on the primary monitor whose's screen X should be 0
+	return 0;
+}
 
+window.leftScreenBoundry = FindLeftScreenBoundry;
+
+function PopupCenter(url, title, w, h) {
+	var newWindow = window.open(url, title, 'resizable=1, scrollbars=1, fullscreen=0, height=' + h + ', width=' + w + ', screenX=' + window.leftScreenBoundry + ' , left=' + window.leftScreenBoundry + ', toolbar=0, menubar=0, status=1');
 	return newWindow;
 }
 
@@ -42,5 +59,5 @@ Keystation.prototype.openWindow = function(type, payload, account = "") {
 			break;
 	}
 
-	return PopupCenter(this.keystationUrl + '/' + apiUrl + '?account=' + encodeURIComponent(account) + '&client=' + encodeURIComponent(this.client) + '&lcd=' + encodeURIComponent(this.lcd) + '&path=' + encodeURIComponent(this.path) + '&payload=' + encodeURIComponent(payload),'','400','690', {toolbar:1, resizable:1, location:1, menubar:1, status:1});
+	return PopupCenter(this.keystationUrl + '/' + apiUrl + '?account=' + encodeURIComponent(account) + '&client=' + encodeURIComponent(this.client) + '&lcd=' + encodeURIComponent(this.lcd) + '&path=' + encodeURIComponent(this.path) + '&payload=' + encodeURIComponent(payload),'','400','690');
 }
